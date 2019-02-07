c630046186957bf8cb12e104b3b02141a268c906
Roman Jędras <romanjedras@o2.pl>
2019-02-07 17:08:57 +0100

    Result Module One

---
+++ scripts.js
@@ -0,0 +1,80 @@
+'use strict';
+
+var output = document.getElementById('output');
+var rock_button = document.getElementById('js-playerPick_rock');
+var paper_button = document.getElementById('js-playerPick_paper');
+var scissors_button = document.getElementById('js-playerPick_scissors');
+
+
+
+
+var playerMove = function(playerPick) {
+    output.innerHTML = playerPick;
+    var computerPlayer;
+    computerPlayer = getComputerPick();
+
+   var gameWinner  = checkRoundWinner(playerPick, computerPlayer);
+   if (gameWinner != 'noone' && gameWinner == 'computer') {
+       output.innerHTML = 'You Won '+ gameWinner + ' played: ' + computerPlayer + ' and player played :' +playerPick;
+   } else if (gameWinner != 'noone' && gameWinner == 'player') {
+       output.innerHTML = 'You Won '+ gameWinner + ' played: ' + playerPick + ' and computer played :' + computerPlayer;
+   }else {
+       output.innerHTML = 'Nobody Winner !!';
+   }
+
+
+}
+
+var random = function() {
+    return Math.floor(Math.random()*3);
+}
+
+var getComputerPick = function() {
+    var randomPick = random();
+    var possiblePicks;
+    if (randomPick == 1) {
+       possiblePicks = 'paper';
+    } else if (randomPick == 2) {
+        possiblePicks = 'rock';
+    }else {
+        possiblePicks = 'scissors';
+    }
+    return possiblePicks;
+    }
+
+var checkRoundWinner = function(playerPick, computerPick) {
+
+    var winnerIs = 'player';
+
+    if (playerPick == computerPick) {
+        return winnerIs = 'noone';
+    } else if (
+        (computerPick == 'rock' &&  playerPick == 'scissors') ||
+        (computerPick == 'scissors' &&  playerPick == 'paper') ||
+        (computerPick == 'paper' &&  playerPick == 'rock')) {
+        return winnerIs = 'computer';
+    }
+
+    if (winnerIs == 'player') {
+       return winnerIs;
+    }
+
+
+}
+
+
+rock_button.addEventListener('click', function () {
+    playerMove(this.name)
+});
+
+paper_button.addEventListener('click', function () {
+    playerMove(this.name)
+});
+
+scissors_button.addEventListener('click', function () {
+    playerMove(this.name)
+});
+
+
+
+
